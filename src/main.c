ae7c59ccb0f556757cdb3a88626df46f505a634d
SimoneFelici <felicisimone9@gmail.com>
2026-03-24 23:59:16 +0100

    only redraw when needed

--- src/main.c
+++ src/main.c
@@ -1,3 +1,5 @@
+#include <SDL3/SDL_events.h>
+#include <SDL3/SDL_init.h>
 #define SDL_MAIN_USE_CALLBACKS 1
 #include <SDL3/SDL_main.h>
 #include "GameHAT-Launcher.h"
@@ -19,7 +21,7 @@ SDL_AppResult SDL_AppInit(void **appstate, int argc, char *argv[])
         return SDL_APP_FAILURE;
     }
 
-    if(!SDL_SetRenderVSync(renderer, 1))
+    if(!SDL_SetRenderVSync(renderer, 2))
     {
         SDL_Log( "Couldn't enable VSync: %s", SDL_GetError() );
         return SDL_APP_FAILURE;
@@ -30,20 +32,29 @@ SDL_AppResult SDL_AppInit(void **appstate, int argc, char *argv[])
     return SDL_APP_CONTINUE;
 }
 
+static bool to_redraw = false;
+
 SDL_AppResult SDL_AppEvent(void *appstate, SDL_Event *event)
 {
     if (event->type == SDL_EVENT_QUIT) {
         return SDL_APP_SUCCESS;
+    }
+    if (event->type == SDL_EVENT_KEY_DOWN) {
+        to_redraw = true;
     }
     return SDL_APP_CONTINUE;
 }
 
 SDL_AppResult SDL_AppIterate(void *appstate)
 {
-    // FPS_Counter();
+    if (!to_redraw)
+        return SDL_APP_CONTINUE;
+    FPS_Counter();
     SDL_SetRenderDrawColor(renderer, 0, 0, 0, SDL_ALPHA_OPAQUE);
     SDL_RenderClear(renderer);
     SDL_RenderPresent(renderer);
+
+    to_redraw = false;
 
     return SDL_APP_CONTINUE;
 }
