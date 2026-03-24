bf3234dde281bb08c65e7a10ca2375f6b8a2e02f
SimoneFelici <felicisimone9@gmail.com>
2026-03-25 00:13:55 +0100

    dirty rendering fix

--- src/main.c
+++ src/main.c
@@ -1,6 +1,7 @@
+#define SDL_MAIN_USE_CALLBACKS 1
+
 #include <SDL3/SDL_events.h>
 #include <SDL3/SDL_init.h>
-#define SDL_MAIN_USE_CALLBACKS 1
 #include <SDL3/SDL_main.h>
 #include "GameHAT-Launcher.h"
 
@@ -47,8 +48,12 @@ SDL_AppResult SDL_AppEvent(void *appstate, SDL_Event *event)
 
 SDL_AppResult SDL_AppIterate(void *appstate)
 {
-    if (!to_redraw)
+    static int init_frames = 5;
+    if (!to_redraw && init_frames <= 0)
         return SDL_APP_CONTINUE;
+
+    if (init_frames > 0) init_frames--;
+
     FPS_Counter();
     SDL_SetRenderDrawColor(renderer, 0, 0, 255, SDL_ALPHA_OPAQUE);
     SDL_RenderClear(renderer);
