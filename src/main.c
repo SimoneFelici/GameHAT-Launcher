40e5b0198f77b0409e023e5417ecb1418835a3bd
SimoneFelici <felicisimone9@gmail.com>
2026-03-25 10:41:30 +0100

    fixes

--- src/main.c
+++ src/main.c
@@ -1,3 +1,5 @@
+#include <SDL3/SDL_oldnames.h>
+#include <SDL3/SDL_render.h>
 #include <SDL3/SDL_video.h>
 #define SDL_MAIN_USE_CALLBACKS 1
 
@@ -28,7 +30,7 @@ SDL_AppResult SDL_AppInit(void **appstate, int argc, char *argv[])
         SDL_Log( "Couldn't enable VSync: %s", SDL_GetError() );
     }
 
-    SDL_SetRenderLogicalPresentation(renderer, 480, 320, SDL_LOGICAL_PRESENTATION_LETTERBOX);
+    SDL_SetRenderLogicalPresentation(renderer, 480, 320, SDL_LOGICAL_PRESENTATION_DISABLED);
 
     return SDL_APP_CONTINUE;
 }
@@ -40,8 +42,10 @@ SDL_AppResult SDL_AppEvent(void *appstate, SDL_Event *event)
     if (event->type == SDL_EVENT_QUIT) {
         return SDL_APP_SUCCESS;
     }
-
-    if (event->type == SDL_EVENT_KEY_DOWN)
+    if (event->type == SDL_EVENT_KEY_DOWN ||
+        event->type == SDL_EVENT_WINDOW_RESIZED ||
+        event->type == SDL_EVENT_WINDOW_PIXEL_SIZE_CHANGED ||
+        event->type == SDL_EVENT_WINDOW_EXPOSED)
         to_draw = true;
 
     return SDL_APP_CONTINUE;
