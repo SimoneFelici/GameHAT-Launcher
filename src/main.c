8a14f360262b692a76468b611cea05d6983bfa21
SimoneFelici <felicisimone9@gmail.com>
2026-03-30 11:28:24 +0200

    scaling

--- src/main.c
+++ src/main.c
@@ -1,6 +1,3 @@
-#include <SDL3/SDL_oldnames.h>
-#include <SDL3/SDL_render.h>
-#include <SDL3/SDL_video.h>
 #define SDL_MAIN_USE_CALLBACKS 1
 
 #include <SDL3/SDL_events.h>
--- src/utils.c
+++ src/utils.c
@@ -1,4 +1,5 @@
 #include "GameHAT-Launcher.h"
+#include <SDL3/SDL_render.h>
 
 void FPS_Counter(SDL_Renderer *renderer) {
     static Uint64 last = 0;
@@ -16,7 +17,9 @@ void FPS_Counter(SDL_Renderer *renderer) {
     }
 
     SDL_SetRenderDrawColor(renderer, 255, 255, 255, SDL_ALPHA_OPAQUE);
+    SDL_SetRenderScale(renderer, 1.5f, 1.5f);
     SDL_RenderDebugText(renderer, 5, 5, fps_text);
-    SDL_Log("%s\n", fps_text);
+    SDL_SetRenderScale(renderer, 1.0f, 1.0f);
+    // SDL_Log("%s\n", fps_text);
 }
 
