ba579b66c89d7efec7325fd7c74122776a65e2bb
SimoneFelici <felicisimone9@gmail.com>
2026-03-30 11:51:42 +0200

    remove callbacks

--- src/main.c
+++ src/main.c
@@ -1,15 +1,9 @@
-#define SDL_MAIN_USE_CALLBACKS 1
-
-#include <SDL3/SDL_events.h>
-#include <SDL3/SDL_init.h>
-#include <SDL3/SDL_main.h>
 #include "GameHAT-Launcher.h"
+#include <SDL3/SDL_init.h>
 
-static SDL_Window *window = NULL;
-static SDL_Renderer *renderer = NULL;
-
-SDL_AppResult SDL_AppInit(void **appstate, int argc, char *argv[])
+int main(int argc, char *argv[])
 {
+    // INIT
     SDL_SetAppMetadata("GameHAT-Launcher", "v0.1", "com.eternalblue.gamehatlauncher");
 
     if (!SDL_Init(SDL_INIT_VIDEO)) {
@@ -17,55 +11,45 @@ SDL_AppResult SDL_AppInit(void **appstate, int argc, char *argv[])
         return SDL_APP_FAILURE;
     }
 
+    SDL_Window *window = NULL;
+    SDL_Renderer *renderer = NULL;
+
     if (!SDL_CreateWindowAndRenderer("Launcher", 480, 320, SDL_WINDOW_FULLSCREEN, &window, &renderer)) {
         SDL_Log("Couldn't create window/renderer: %s", SDL_GetError());
         return SDL_APP_FAILURE;
     }
 
-    if(!SDL_SetRenderVSync(renderer, 1))
-        SDL_Log( "Couldn't enable VSync: %s", SDL_GetError() );
-
+    if (!SDL_SetRenderVSync(renderer, 1))
+        SDL_Log("Couldn't enable VSync: %s", SDL_GetError());
     if (!SDL_HideCursor())
-        SDL_Log( "Couldn't hide cursor: %s", SDL_GetError() );
-
+        SDL_Log("Couldn't hide cursor: %s", SDL_GetError());
 
     SDL_SetRenderLogicalPresentation(renderer, 480, 320, SDL_LOGICAL_PRESENTATION_DISABLED);
 
-    return SDL_APP_CONTINUE;
-}
-
-static bool to_draw = true;
-
-SDL_AppResult SDL_AppEvent(void *appstate, SDL_Event *event)
-{
-    if (event->type == SDL_EVENT_QUIT) {
-        return SDL_APP_SUCCESS;
-    }
-    if (event->type == SDL_EVENT_KEY_DOWN ||
-        event->type == SDL_EVENT_WINDOW_RESIZED ||
-        event->type == SDL_EVENT_WINDOW_PIXEL_SIZE_CHANGED ||
-        event->type == SDL_EVENT_WINDOW_EXPOSED)
-        to_draw = true;
-
-    return SDL_APP_CONTINUE;
-}
-
-SDL_AppResult SDL_AppIterate(void *appstate)
-{
-    if (!to_draw)
-        return SDL_APP_CONTINUE;
-
+    // MAIN
     SDL_SetRenderDrawColor(renderer, 0, 0, 255, SDL_ALPHA_OPAQUE);
     SDL_RenderClear(renderer);
     FPS_Counter(renderer);
     SDL_RenderPresent(renderer);
 
-    to_draw = false;
+    SDL_Event event;
+    while (SDL_WaitEvent(&event)) {
+        if (event.type == SDL_EVENT_QUIT)
+            break;
 
-    return SDL_APP_CONTINUE;
+        if (event.type == SDL_EVENT_KEY_DOWN ||
+            event.type == SDL_EVENT_WINDOW_RESIZED ||
+            event.type == SDL_EVENT_WINDOW_PIXEL_SIZE_CHANGED ||
+            event.type == SDL_EVENT_WINDOW_EXPOSED)
+        {
+            SDL_SetRenderDrawColor(renderer, 0, 0, 255, SDL_ALPHA_OPAQUE);
+            SDL_RenderClear(renderer);
+            FPS_Counter(renderer);
+            SDL_RenderPresent(renderer);
+        }
+    }
+
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
 }
-
-void SDL_AppQuit(void *appstate, SDL_AppResult result)
-{
-}
-
