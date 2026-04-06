24117b73208abc26e03317bd37d137c491312e13
SimoneFelici <felicisimone9@gmail.com>
2026-04-06 18:52:23 +0200

    comment

--- build.sh
+++ build.sh
@@ -3,4 +3,6 @@
 mkdir -p out/
 gcc src/*.c -O3 -I/usr/include/SDL3 -lSDL3 -o out/GameHAT-Launcher
 sudo cp out/GameHAT-Launcher /usr/local/bin/
+
+# Workaround to stop writing on the TTY
 sudo setcap cap_sys_tty_config+ep /usr/local/bin/GameHAT-Launcher
