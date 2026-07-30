0cf3a9f40b6a11ebaa0f56f62902335c4f1015e9
SimoneFelici <felicisimone9@gmail.com>
2026-07-30 18:21:57 +0200

    list games

--- src/main.zig
+++ src/main.zig
@@ -5,6 +5,8 @@ const Context = @import("context.zig").Context;
 pub fn main(init: std.process.Init) anyerror!void {
     const screenWidth = 800;
     const screenHeight = 450;
+    const font_size: f32 = 20;
+    const spacing: f32 = 2;
 
     var ctx: Context = .{
         .io = init.io,
@@ -27,10 +29,13 @@ pub fn main(init: std.process.Init) anyerror!void {
 
         rl.clearBackground(config.bg_color);
 
-        var y: i32 = 40;
-        for (ctx.games.items) |game| {
-            rl.drawText(game.name, 40, y, 20, config.txt_color);
-            y += 30;
+        for (ctx.games.items, 0..) |game, i| {
+            const size = rl.measureTextEx(try rl.getFontDefault(), game.name, font_size, spacing);
+            const pos: rl.Vector2 = .{
+                .x = (@as(f32, screenWidth) - size.x) / 2,
+                .y = @as(f32, @floatFromInt(i)) * (size.y + 5),
+            };
+            rl.drawTextEx(try rl.getFontDefault(), game.name, pos, font_size, spacing, config.txt_color);
         }
     }
 }
